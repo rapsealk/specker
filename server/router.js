323decb7a7e1e9e38a42030b33f62b8b0472f435
jeon sanghyun <sanghyunJ@jeons-MacBook-Pro.local>
2016-08-18 23:29:20 +0900

    update launch and create classification

---
+++ server/router.js
@@ -0,0 +1,23 @@
+const Authentication = require('./controllers/authentication');
+const Classification = require('./controllers/classification');
+
+const passportService = require('./services/passport');
+const passport = require('passport');
+
+const requireAuth = passport.authenticate('jwt', { session: false });
+const requireSignin = passport.authenticate('local', { session: false });
+
+module.exports = function(app) {
+    app.get('/', requireAuth, function(req, res) {
+        res.send({ message: 'Super secret code is ABC123' });
+    });
+    app.post('/signin', requireSignin, Authentication.signin);
+    app.post('/signup', Authentication.signup);
+
+
+    app.post('/getClassification', requireAuth, Classification.getClassification);
+
+
+    app.post('/saveClassification', requireAuth, Classification.saveClassification);
+
+};
