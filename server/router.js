07997a2b3b94a648a86d1a929794d45436ac3d3f
jeon sanghyun <sanghyunJ@jeons-MacBook-Pro.local>
2016-08-29 20:29:58 +0900

    test

--- server/router.js
+++ server/router.js
@@ -14,9 +14,7 @@ module.exports = function(app) {
     app.post('/signin', requireSignin, Authentication.signin);
     app.post('/signup', Authentication.signup);
 
-
     app.post('/getClassification', requireAuth, Classification.getClassification);
-
 
     app.post('/saveClassification', requireAuth, Classification.saveClassification);
 
