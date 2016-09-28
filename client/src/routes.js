3e3144ac41b0445e8a21448ca50bb0fb3edc6d2a
jjh4698 <wjdwngur@gmail.com>
2016-09-28 17:51:24 +0900

    mainpage geometric

--- client/src/routes.js
+++ client/src/routes.js
@@ -10,10 +10,12 @@ import Launch from './components/launch';
 import Classification from './components/classification';
 import Index from './components/index';
 import HomeBody from './containers/home/home-body';
+import Google_Map from './components/google_map';
 
 export default(
     <Route path="/" component={App} >
         <IndexRoute component={Launch} />
+        <Route path="/google" component={Google_Map}/>
         <Route path="/classification" component={RequireAuth(Classification)}/>
         <Route path="/home" component={RequireTagAuth(Index)}>
             <IndexRoute component={HomeBody} />
